4dca7dcf87b93009d514ba0e3fb8cc8b39043e83
ganesh <ganiiscool@gmail.com>
2022-12-08 23:02:53 +0530

    adding files

--- app.js
+++ app.js
@@ -8,7 +8,9 @@ var googleTranslate = require("google-translate")(process.env.API_KEY);
 const app = express();
 app.use(bodyParser.json());
 // app.use(express.static("public"));
-
+app.get('/',(req,res)=>{
+    res.send('App is running')
+})
 app.post("/detect", (req, res) => {
   googleTranslate.detectLanguage(req.body.text, function (err, detection) {
     res.send({data:detection});
